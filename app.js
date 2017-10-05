d5d956d53986279f34930feff6594f1c704dcf91
boyuanl1 <boyuanl1@icloud.com>
2017-10-04 17:21:19 -0700

    fixed spotify token

--- app.js
+++ app.js
@@ -41,16 +41,7 @@ var spotifyApi = new SpotifyWebApi({
 	  redirectUri : 'http://www.example.com/callback'
 	});
 
-spotifyApi.clientCredentialsGrant()
-.then(function(data) {
-console.log('The access token expires in ' + data.body['expires_in']);
-console.log('The access token is ' + data.body['access_token']);
 
-// Save the access token so that it's used in future calls
-spotifyApi.setAccessToken(data.body['access_token']);
-}, function(err) {
-console.log('Something went wrong when retrieving an access token', err.message);
-});
 
 var pm = new PlayMusic();
 
@@ -78,20 +69,31 @@ app.post("/search", function(req, res) {
 		
 	})	
 //Spotify-------------------------------------------------------
-	spotifyApi.searchTracks(JSON.parse(req.body).song, {limit: 10})
+	spotifyApi.clientCredentialsGrant()
 	.then(function(data) {
-		result.spotify = [];
-		for (var i = 0; i < data.body.tracks.items.length; i++) {
-			var item = {
-					song: data.body.tracks.items[i].name,
-					artist: data.body.tracks.items[i].artists[0].name,
-					album: data.body.tracks.items[i].album.name,
-					img: data.body.tracks.items[i].album.images[1].url,
-					url: data.body.tracks.items[i].external_urls.spotify,
-					preview: null
+//		console.log('The access token expires in ' + data.body['expires_in']);
+//		console.log('The access token is ' + data.body['access_token']);
+	
+		// Save the access token so that it's used in future calls
+		spotifyApi.setAccessToken(data.body['access_token']);
+		
+		spotifyApi.searchTracks(JSON.parse(req.body).song, {limit: 10})
+		.then(function(data) {
+			result.spotify = [];
+			for (var i = 0; i < data.body.tracks.items.length; i++) {
+				var item = {
+						song: data.body.tracks.items[i].name,
+						artist: data.body.tracks.items[i].artists[0].name,
+						album: data.body.tracks.items[i].album.name,
+						img: data.body.tracks.items[i].album.images[1].url,
+						url: data.body.tracks.items[i].external_urls.spotify,
+						preview: null
+				}
+				result.spotify.push(item)
 			}
-			result.spotify.push(item)
-		}
+	}, function(err) {
+		console.log('Something went wrong when retrieving an access token', err.message);
+	});
 		
 		
 //	  res.json(data)
