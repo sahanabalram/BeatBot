922ca5bc21f32c8cc5232152c0648bbd0920fe40
Sahana Balram <sahana2008balram@gmail.com>
2017-10-06 05:24:06 -0700

    Run test for the routes

--- test/test.js
+++ test/test.js
@@ -5,10 +5,53 @@ var request = require('request');
 // Home Page test
 
 describe("Home Page  Test", function () {
-   var url = "http://localhost:4005/";
-      it("returns status 200", function () {
+    var url = "http://localhost:4005/";
+    it("returns status 200", function () {
         request(url, function (error, response, body) {
             expect(response.statusCode).to.equal(200);
+        });
+    });
+});
+
+// Sign-Up page test
+
+describe("Sign-Up Page Loading Test", function () {
+    var url = "http://localhost:4005/signup";
+    it("returns status 200", function () {
+        request(url, function (error, response, body) {
+            expect(response.statusCode).to.equal(200);
+        });
+    });
+});
+
+
+// User Page test
+
+describe("Home Page Loading Test", function () {
+    var url = "http://localhost:3100/user";
+    it("returns status 200", function () {
+        request(url, function (error, response, body) {
+            expect(response.statusCode).to.equal(200);
+        });
+    });
+});
+
+// Sign Up and Create User in Database
+describe("User sign up", function() {
+    var url = "http://localhost:4005/signup";
+    it("signs up a user", function() {
+        request({
+            uri: url,
+            method: 'POST',
+            json: {
+                username: "Sahana2008",
+                password: "password2009",
+                email: "rsahana@gmail.com",
+            }
+        }, function(error, body, response) {
+            if (error) throw error;
+            expect(response.statusCode).to.equal(302);
+
         });
     });
 });
