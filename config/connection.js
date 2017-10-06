2d8d393605e6f9fa9d182f1bb4b9f0ccbc70e7e2
Sahana Balram <sahana2008balram@gmail.com>
2017-10-05 23:44:59 -0700

    Heroku

---
+++ config/connection.js
@@ -0,0 +1,10 @@
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'beatBot_db'
+});
+};
